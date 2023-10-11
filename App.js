2049588a29c19ea1d1e2fbfb66b8c9283929546e
jibon969 <jayed.swe@gmail.com>
2023-10-11 18:13:40 +0600

    added navigation

--- App.js
+++ App.js
@@ -1,24 +1,44 @@
-import React from 'react'
-import { SafeAreaView, View, StatusBar, StyleSheet, Text } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <View>
+//       <Text>App</Text>
+//     </View>
+//   )
+// }
+
+// export default App
+
+// const styles = StyleSheet.create({})
+
+import React, {createRef} from 'react'
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+
 import DrawerNavigator from './src/navigation/DrawerNavigator'
 
+// store reference to navigation object
+const navigationRef = createRef();
+const nav = () => navigationRef.current;
+
 const App = () => {
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <StatusBar barStyle="dark-content" />
-      <NavigationContainer>
-          <DrawerNavigator/>
-      </NavigationContainer>
-    </SafeAreaView>
-  )
-}
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <StatusBar backgroundColor="#551E18" barStyle="light-content"/>
+            <NavigationContainer ref={navigationRef}>
+                <DrawerNavigator nav={nav}/>
+            </NavigationContainer>
+        </SafeAreaView>
+    )
+};
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    overflow: 'hidden',
-  },
-})
+    safeArea: {
+        flex: 1,
+        overflow: 'hidden',
+    },
+});
 
 export default App
--- android/app/src/main/java/com/combiningnavigationcli/MainApplication.java
+++ android/app/src/main/java/com/combiningnavigationcli/MainApplication.java
@@ -1,4 +1,4 @@
-package com.combiningnavigationstack;
+package com.combiningnavigationcli;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
